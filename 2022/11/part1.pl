cce417b803b8dd88a58de603bd38357445b181bb
boblied <boblied@gmail.com>
2022-12-11 10:36:19 -0600

    part 1

---
+++ 2022/11/part1.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+# vim:set ts=4 sw=4 sts=4 et ai wm=0 nu:
+#=============================================================================
+# part1.pl
+#=============================================================================
+# Copyright (c) 2022, Bob Lied
+#=============================================================================
+
+use v5.36;
+
+use lib "lib";
+use Monkey;
+
+my @Monkey;
+
+sub readInput
+{
+    local $/ = ""; # Paragraph at a time
+    push @Monkey, Monkey::parse($_) while (<> );
+}
+
+readInput();
+Monkey::setCollection(\@Monkey);
+
+say scalar(@Monkey), " monkeys";
+
+for my $round ( 1 .. 20 )
+{
+
+    for my $monkey ( @Monkey )
+    {
+        say "MONKEY ", $monkey->id;
+        for my $m ( @Monkey ) { say "  ROUND $round BEFORE: ", $m->showList; }
+        $monkey->takeTurn();
+        for my $m ( @Monkey ) { say "  ROUND $round AFTER:  ", $m->showList; }
+    }
+}
+
+for my $m (@Monkey )
+{
+    say "Monkey ".$m->id." inspected items ".$m->inspectCount." times";
+}
+
+my @sorted = sort { $b <=> $a } map { $_->inspectCount } @Monkey;
+
+say "$sorted[0] * $sorted[1] = ", $sorted[0] * $sorted[1];
