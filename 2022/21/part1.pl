9879a5e23bdd31f944ec0bade6af7f802c34d8ce
boblied <boblied@gmail.com>
2022-12-21 07:41:10 -0600

    start hacking

--- 2022/21/part1.pl
+++ 2022/21/part1.pl
@@ -6,4 +6,34 @@
 # Copyright (c) 2022, Bob Lied
 #=============================================================================
 
+# This is going to be a recursive-descent parser. Eventually.
+
 use v5.36;
+use lib ".";
+
+use Parse;
+
+readInput();
+
+my %SymbolTable;
+my %Parser;
+
+
+sub readInput()
+{
+    while (<>)
+    {
+        chomp;
+        my ($var, $left, $op, $right) = m/(\w+): (\w+) ([-\+\*\/]) (\w+)/;
+
+        if ( not defined $op )
+        {
+            my $(name, $value) = m/(\w+): (\d+)/;
+            $SymbolTable{$var} = $left;
+        }
+        else
+        {
+            $Parser{$var} = Parse->new(name => $var, left => $left, op => $op, right => $right);
+        }
+    }
+}
