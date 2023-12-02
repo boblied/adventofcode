529799fa38104fc6ebaf66cff213eccb3f3613a8
Bob Lied <boblied+github@gmail.com>
2023-12-02 16:17:58 -0600

    AOC 2021 day 03 part 1 done

---
+++ 2023/03/part2.pl
@@ -0,0 +1,17 @@
+#!/usr/bin/env perl
+# vim:set ts=4 sw=4 sts=4 et ai wm=0 nu:
+#=============================================================================
+# part1.pl Advebt of Code 2023 Day 03 Part 1
+#=============================================================================
+# Copyright (c) 2023, Bob Lied
+#=============================================================================
+# 
+#=============================================================================
+
+use v5.38;
+use builtin qw/true false/; no warnings "experimental::builtin";
+
+use Getopt::Long;
+my $Verbose = 0;
+
+GetOptions("verbose" => \$Verbose);
