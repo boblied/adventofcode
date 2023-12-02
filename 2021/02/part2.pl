2fd42dc5b16593407ab957c4b893e906fb305895
Bob Lied <boblied+github@gmail.com>
2023-12-02 16:22:46 -0600

    AOC 2021 Day 02 complete

--- 2021/02/part2.pl
+++ 2021/02/part2.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 # vim:set ts=4 sw=4 sts=4 et ai wm=0 nu:
 #=============================================================================
-# part1.pl Advebt of Code 2021 Day 02 Part 1
+# part1.pl Advebt of Code 2021 Day 02 Part 2
 #=============================================================================
 # Copyright (c) 2023, Bob Lied
 #=============================================================================
@@ -17,12 +17,13 @@ my $Verbose = 0;
 GetOptions("verbose" => \$Verbose);
 my $horiz = 0;
 my $depth = 0;
+my $aim   = 0;
 while (<>)
 {
     chomp;
     my ($dir, $val) = split " ";
-    if    ( m/forward/ ) { $horiz += $val }
-    elsif ( m/up/      ) { $depth -= $val }
-    elsif ( m/down/    ) { $depth += $val }
+    if    ( m/down/    ) { $aim += $val }
+    elsif ( m/up/      ) { $aim -= $val }
+    elsif ( m/forward/ ) { $horiz += $val; $depth += ($val * $aim); }
 }
 say $horiz * $depth;
