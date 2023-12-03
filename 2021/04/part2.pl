259a1f5ef3ae815c46daf075e39345e9b65d8016
Bob Lied <boblied+github@gmail.com>
2023-12-03 11:42:51 -0600

    AOC 2021 Day 4 prep

---
+++ 2021/04/part2.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/env perl
+# vim:set ts=4 sw=4 sts=4 et ai wm=0 nu:
+#=============================================================================
+# part1.pl Advent of Code 2021 Day 04 Part 2
+#=============================================================================
+# Copyright (c) 2023, Bob Lied
+#=============================================================================
+# 
+#=============================================================================
+
+use v5.38;
+use builtin qw/true false/; no warnings "experimental::builtin";
+
+use Getopt::Long;
+my $Verbose = 0;
+my $DoTest  = 0;
+
+GetOptions("test" => \$DoTest, "verbose" => \$Verbose);
+exit(!runTest()) if $DoTest;
+
+sub runTest
+{
+    use Test2::V0;
+
+    is(0, 1, "FAIL");
+
+    done_testing;
+}
