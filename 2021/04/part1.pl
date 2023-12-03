077ed5ebcd11204a9a3603e32406cf302b1898d3
Bob Lied <boblied+github@gmail.com>
2023-12-03 12:33:13 -0600

    AOC 2021 04 1 complete

--- 2021/04/part1.pl
+++ 2021/04/part1.pl
@@ -11,12 +11,84 @@
 use v5.38;
 use builtin qw/true false/; no warnings "experimental::builtin";
 
+use Data::Dumper; $Data::Dumper::Sortkeys = 1;
+
+use List::Util qw/all sum/;
+
 use Getopt::Long;
 my $Verbose = 0;
 my $DoTest  = 0;
 
 GetOptions("test" => \$DoTest, "verbose" => \$Verbose);
 exit(!runTest()) if $DoTest;
+
+my @Move;
+my @Board;
+my @Number;
+{
+    local $/ = "";
+    my $mv = <>;
+    chomp($mv);
+    @Move = split(",", $mv);
+    # say "Moves: ", Dumper(\@Move);
+
+    while (<> )
+    {
+        chomp;
+        onBoard($_, \@Board, \@Number);
+    }
+
+    say "Done loading" if $Verbose;
+}
+
+say play(\@Board, \@Move, \@Number);
+
+sub score($board, $move)
+{
+    my $sum = sum grep { $_ ne "X" } map { $_->@* } $board->@*;
+    return $sum * $move;
+}
+
+sub play($board, $move, $number)
+{
+    for my $m ( $move->@* )
+    {
+        for ( $number->[$m]->@* )
+        {
+            my ($b, $r, $c) = $_->@*;
+            say "$m on board $b at ($r, $c)" if $Verbose;
+
+            $board->[$b][$r][$c] = "X";
+
+            my @row = $board->[$b][$r]->@*;
+            return score($board->[$b], $m) if ( all { $_ eq "X" } @row );
+
+            my @col = map { $board->[$b][$_][$c] } 0 .. 4;
+            return score($board->[$b], $m) if ( all { $_ eq "X" } @col );
+        }
+    }
+}
+
+sub onBoard($str, $board, $number)
+{
+    my @b;
+    for my $row ( split("\n", $str) )
+    {
+        push @b, [ split(" ", $row) ];
+    }
+    #say "board", Dumper(\@b);
+    push @{$board}, \@b;
+
+    # For each number on the board, remember its location
+    for my $r ( 0 .. $#b )
+    {
+        for my $c ( 0 .. $b[$r]->$#* )
+        {
+            my $n = $b[$r][$c];
+            push @{$number->[$n]}, [ $board->$#*, $r, $c ];
+        }
+    }
+}
 
 sub runTest
 {
