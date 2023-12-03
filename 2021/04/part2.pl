08ab152bba81bc00efea5a928a9198dc12069575
Bob Lied <boblied+github@gmail.com>
2023-12-03 15:34:47 -0600

    AOC 2021 04 2 complete

--- 2021/04/part2.pl
+++ 2021/04/part2.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/env perl
 # vim:set ts=4 sw=4 sts=4 et ai wm=0 nu:
 #=============================================================================
-# part1.pl Advent of Code 2021 Day 04 Part 2
+# part1.pl Advent of Code 2021 Day 04 Part 1
 #=============================================================================
 # Copyright (c) 2023, Bob Lied
 #=============================================================================
@@ -11,18 +11,94 @@
 use v5.38;
 use builtin qw/true false/; no warnings "experimental::builtin";
 
+use Data::Dumper; $Data::Dumper::Sortkeys = 1;
+
+use List::Util qw/all sum/;
+
 use Getopt::Long;
 my $Verbose = 0;
-my $DoTest  = 0;
 
-GetOptions("test" => \$DoTest, "verbose" => \$Verbose);
-exit(!runTest()) if $DoTest;
+GetOptions("verbose" => \$Verbose);
 
-sub runTest
+my @Move;
+my @Board;
+my @Number;
+my $BoardsInPlay;
+
 {
-    use Test2::V0;
+    local $/ = "";
+    my $mv = <>;
+    chomp($mv);
+    @Move = split(",", $mv);
+    # say "Moves: ", Dumper(\@Move);
 
-    is(0, 1, "FAIL");
+    while (<> )
+    {
+        chomp;
+        onBoard($_, \@Board, \@Number);
+    }
 
-    done_testing;
+    $BoardsInPlay = scalar(@Board);
+
+    say "Done loading" if $Verbose;
+}
+
+say play(\@Board, \@Move, \@Number);
+
+sub score($board, $move)
+{
+    my $sum = sum grep { $_ ne "X" } map { $_->@* } $board->@*;
+    return $sum * $move;
+}
+
+sub play($board, $move, $number)
+{
+    for my $m ( $move->@* )
+    {
+        for ( $number->[$m]->@* )
+        {
+            my ($b, $r, $c) = $_->@*;
+            # say "$m on board $b at ($r, $c)" if $Verbose;
+            next if ! defined $board->[$b];
+
+            $board->[$b][$r][$c] = "X";
+
+            my @row = $board->[$b][$r]->@*;
+            my @col = map { $board->[$b][$_][$c] } 0 .. 4;
+            if (  ( all { $_ eq "X" } @row )
+               || ( all { $_ eq "X" } @col ) )
+            {
+                $BoardsInPlay--;
+                say "Board $b wins, $BoardsInPlay left" if $Verbose;
+
+                if ( $BoardsInPlay == 0 )
+                {
+                    say "Board $b is last" if $Verbose;
+                    return score($board->[$b], $m);
+                }
+                $board->[$b] = undef;
+            }
+        }
+    }
+}
+
+sub onBoard($str, $board, $number)
+{
+    my @b;
+    for my $row ( split("\n", $str) )
+    {
+        push @b, [ split(" ", $row) ];
+    }
+    #say "board", Dumper(\@b);
+    push @{$board}, \@b;
+
+    # For each number on the board, remember its location
+    for my $r ( 0 .. $#b )
+    {
+        for my $c ( 0 .. $b[$r]->$#* )
+        {
+            my $n = $b[$r][$c];
+            push @{$number->[$n]}, [ $board->$#*, $r, $c ];
+        }
+    }
 }
