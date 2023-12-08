64cf9eceed76718d7b87174e61a16c4bb069c22c
Bob Lied <boblied+github@gmail.com>
2023-12-08 09:25:52 -0600

    AOC 2023 Day 8 complete

--- 2023/08/part2.pl
+++ 2023/08/part2.pl
@@ -38,24 +38,61 @@ say "Start: @Start" if $Verbose;
 
 sub allZ($nodes) { ($nodes->$#* + 1 ) == grep /Z\Z/, $nodes->@* }
 
+my @ZLOC;
+$ZLOC[$_] = [] for 0 .. $#Start;
+sub showZLOC($str)
+{
+    say $str;
+    for my $z ( @ZLOC )
+    {
+        printf(("[ " . ("%12d" x scalar($z->@*)) . "]\n"), $z->@*);
+    }
+}
+
+# Each start point will eventually reach a Z, and the input is
+# rigged so that it cycles through them.  Once we've found the
+# cycle for each start point, we can determine where they will
+# eventually line up by finding the least common multiple.
+my @CYCLE = ( (0) x scalar(@Start) );
+
 my $step = 0;
 my $count = 0;
-while ( ! allZ(\@Node) )
+STEP: while ( ! allZ(\@Node) )
 {
     my $dir = $Step[$step++];
     $step = 0 if $step == scalar(@Step);
     $count++; print STDERR "." if ($count % 1000000) == 0;
 
-    @Node = map { $_->{$dir} } @Graph{@Node};
-    # say "$count [ @Start ] -$dir-> [ @Node ]" if $Verbose;
-    if ( $Verbose )
+    @Node = map { $_->{$dir} } @Graph{@Node}; # Ain't that cute?
+
+    if ( grep /Z/, "@Node" )
     {
-        my $zCount = grep /Z\Z/, @Node;
-        say "Z=$zCount at $count for [ @Node ]" if $Verbose and $zCount > 2;
+        for ( 0 .. $#Node )
+        {
+            if ( substr($Node[$_], -1, 1) eq 'Z' )
+            {
+                push @{$ZLOC[$_]}, $count;
+
+                if ( $ZLOC[$_]->$#* == 1 ) # Second time we saw a Z
+                {
+                    if ( $ZLOC[$_][1] - $ZLOC[$_][0] == $ZLOC[$_][0] )
+                    {
+                        say "Found cycle for $_ at $ZLOC[$_][0]";
+                        $CYCLE[$_] = $ZLOC[$_][0];
+                        last STEP if all {$_ > 0 } @CYCLE;
+                    }
+                }
+            }
+        }
+        # showZLOC("Found Z at $count: @Node")
     }
 }
 say "@Node" if $Verbose;
-say $count;
+say "CYCLE: @CYCLE";
+use Math::Utils qw/lcm/;
+say lcm(@CYCLE);
+# CYCLE: 13939 19199 18673 12361 11309 16043
+# 8906539031197
 
 ########################################
 sub runTest
