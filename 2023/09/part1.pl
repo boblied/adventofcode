6b5f9ce8937cf61e5ce3e0453c5313e8ab12cc09
Bob Lied <boblied+github@gmail.com>
2023-12-09 14:47:13 -0600

    AOC 2023 Day 09 complete

--- 2023/09/part1.pl
+++ 2023/09/part1.pl
@@ -9,7 +9,8 @@
 #=============================================================================
 
 use v5.38;
-use builtin qw/true false/; no warnings "experimental::builtin";
+use builtin qw/trim true false/; no warnings "experimental::builtin";
+use List::Util qw/all/;
 
 use Log::Log4perl qw(:easy);
 
@@ -18,21 +19,55 @@ my $DoTest  = 0;
 my $DoDebug = 0;
 
 my $logger = Log::Log4perl->get_logger();
-Log::Log4perl->easy_init($INFO);
-$logger->info("START");
+Log::Log4perl->easy_init($WARN);
 my %DBLEVEL = ( 1 => $INFO, 2 => $DEBUG, 3 => $TRACE,
                 i => $INFO, d => $DEBUG, t => $TRACE, );
 
-GetOptions("test" => \$DoTest, "debug:i" => \$DoDebug);
-say "$DoDebug, $DBLEVEL{$DoDebug}";
+GetOptions("test" => \$DoTest, "debug:s" => \$DoDebug);
 $logger->level($DBLEVEL{$DoDebug}) if $DoDebug;
 exit(!runTest()) if $DoTest;
 
 sub runTest
 {
     use Test2::V0;
-
     is(0, 1, "FAIL");
-
     done_testing;
+}
+
+$logger->info("START");
+
+my $Sum = 0;
+while ( <> )
+{
+    #### PART 2 adds the reverse.  Part 1 works without the reverse.
+    my @seq = reverse split " ";
+    INFO "@seq";
+    $Sum += extrapolate(\@seq);
+}
+say $Sum;
+
+sub extrapolate($seq)
+{
+    my @diffStack = ( $seq );
+    while ( ! ($seq->[0] == $seq->[-1] && all { $_ == $seq->[0] } $seq->@* ) )
+    {
+        DEBUG "seq: [ $seq->@* ]";
+        push @diffStack, [ map { $seq->[$_] - $seq->[$_-1] } 1 .. ($seq->$#*) ];
+        $seq = $diffStack[-1];
+    }
+    my $d = $diffStack[-1][-1];
+    DEBUG "const row, d=$d";
+    $seq = pop @diffStack;
+
+    my $extrapolation;
+    while ( $seq = pop @diffStack )
+    {
+        $extrapolation = $seq->[-1] + $d;
+        push @$seq, $extrapolation;
+        DEBUG "POP: d=$d seq is now $seq->@*";
+        $d = $extrapolation;
+    }
+
+    DEBUG "extrapolate returns $extrapolation";
+    return $extrapolation;
 }
