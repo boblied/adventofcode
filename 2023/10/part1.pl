6e0a9cc27616db00e51ac2d043429a8c37fbb8b9
Bob Lied <boblied+github@gmail.com>
2023-12-09 16:14:36 -0600

    AOC Day 10 Part 1 set up perl file

---
+++ 2023/10/part1.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/env perl
+# vim:set ts=4 sw=4 sts=4 et ai wm=0 nu:
+#=============================================================================
+# part1.pl Perl Weekly Challenge Task  
+#=============================================================================
+# Copyright (c) 2023, Bob Lied
+#=============================================================================
+# 
+#=============================================================================
+
+use v5.38;
+use builtin qw/true false/; no warnings "experimental::builtin";
+use FindBin qw($Bin); use lib "$FindBin::Bin/../../lib"; use AOC;
+AOC::setup;
+
+$logger->info("START");
+
+$logger->info("FINISH");
