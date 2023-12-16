9ec16e0603d229160e6628e98c446107c071eefd
Bob Lied <boblied+github@gmail.com>
2023-12-16 07:17:50 -0600

    Add AOC::Grid object

--- lib/AOC/Grid.pm
+++ lib/AOC/Grid.pm
@@ -13,21 +13,56 @@ use v5.38;
 
 require Exporter;
 our @ISA = qw(Exporter);
-our @EXPORT = qw(showAofS transposeAofS);
-our @EXPORT_OK = qw();
+our @EXPORT = qw(loadFile);
+our @EXPORT_OK = qw(loadFile);
 
-
-# Transpose a grid that's represented as an array of strings
-sub transposeAofS($m)
+sub new
 {
-    my @t;
-    my $width = length($m->[0]);
+    my $class = shift;
+    $class = ref($class) || $class;
+    my $self = {
+        _grid => [],
+        _height => 0,
+        _width => 0,
+    };
+    bless $self, $class;
+    return $self;
+}
 
-    for (my $c = 0 ; $c < $width ; $c++ )
+sub grid($self) { return $self->{_grid} }
+sub height($self) { return $self->{_height} }
+sub width($self) { return $self->{_width} }
+
+sub loadFile
+{
+    my $g = AOC::Grid->new();
+    while (<>)
     {
-        push @t, join "", map { substr($_, $c, 1) } $m->@*;
+        chomp;
+        push @{$g->{_grid}}, [ split // ];
     }
-    return \@t;
+    $g->{_height} = $g->{_grid}->$#*;
+    $g->{_width}  = $g->{_grid}->[0]->$#*;
+    return $g;
+}
+
+sub show($self)
+{
+    my $grid = $self->{_grid};
+    my $height = $self->{_height};
+    my $width  = $self->{_width};
+    my $colFormat = " %2s" x ($width+1);
+    my $s = "\n";
+
+    $s .= sprintf("     $colFormat\n", 0 .. $width);
+    $s .=         "   + " . (" --" x ($width+1)) . "+\n";
+    for my $row ( 0 .. $height )
+    {
+        $s .= sprintf(" %2s |$colFormat | %-2s\n", $row, $grid->[$row]->@*, $row);
+    }
+    $s .=          "   + " . (" --" x ($width+1)) . "+\n";
+    $s .= sprintf( "     $colFormat\n", 0 .. $width);
+    return $s;
 }
 
 1;
